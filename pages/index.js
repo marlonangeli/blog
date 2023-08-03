29d7ac1c83bbf4706959c8b1565e31d156e22c41
Marlon Angeli <marlonangeli91@gmail.com>
2023-08-03 17:08:57 -0300

    update main page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,22 @@
+import Link from "next/link";
+
 function Home() {
-  return <h1>Hello World!</h1>;
+  return (
+    <>
+      <div class="flex justify-center items-center min-h-screen bg-gray-100">
+        <div class="max-w-sm p-12 bg-white rounded-lg shadow-lg">
+          <h2 class="text-2xl font-bold mb-2">Vem aí um projeto daora</h2>
+          <p class="text-gray-600">
+            Essa é a primeira página da implementação do{" "}
+            <Link href="https://tabnews.com.br" className="underline">
+              TabNews
+            </Link>{" "}
+            para o <strong>curso.dev</strong>
+          </p>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Home;
