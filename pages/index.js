3f79fecb993882c4acb4c20a21c2717e18240489
Marlon Angeli <iam@marlonangeli.com.br>
2023-12-09 21:18:38 -0300

    feat: add Sonar Lint suggestions

--- pages/index.js
+++ pages/index.js
@@ -2,20 +2,18 @@ import Link from "next/link";
 
 function Home() {
   return (
-    <>
-      <div class="flex justify-center items-center min-h-screen bg-gray-100">
-        <div class="max-w-sm p-12 bg-white rounded-lg shadow-lg">
-          <h2 class="text-2xl font-bold mb-2">Vem aí um projeto daora</h2>
-          <p class="text-gray-600">
-            Essa é a primeira página da implementação do{" "}
-            <Link href="https://tabnews.com.br" className="underline">
-              TabNews
-            </Link>{" "}
-            para o <strong>curso.dev</strong>
-          </p>
-        </div>
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="max-w-sm p-12 bg-white rounded-lg shadow-lg">
+        <h2 className="text-2xl font-bold mb-2">Vem aí um projeto daora</h2>
+        <p className="text-gray-600">
+          Essa é a primeira página da implementação do{" "}
+          <Link href="https://tabnews.com.br" className="underline">
+            TabNews
+          </Link>{" "}
+          para o <strong>curso.dev</strong>
+        </p>
       </div>
-    </>
+    </div>
   );
 }
 
